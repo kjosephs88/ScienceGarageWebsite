ec671acb15c20031555fde316d3e6b4fa568d747
Keith Josephs <keithdjosephs@yahoo.com>
2026-05-13 12:07:54 -0400

    pics for 08_21

--- functions/keys.js
+++ functions/keys.js
@@ -65,5 +65,52 @@ module.exports = {
     11: 1, // hydrogen
     12: 2, // Arrhenius is also BL and Lewis
     13: 1  // Arrhenius
+  },
+  "CH_U08_A19_G": {
+    1: 3, // hydrogen
+    2: 1, // an acid
+    3: 3, // H+ as the only positive ion
+    4: 1, // hydroxide
+    5: 2, // OH- ions
+    6: 0, // contains more OH- than H+
+    7: 2, // NaOH
+    8: 0, // LiOH
+    9: 2, // a proton donor, only
+    10: 2, // an acid, because it donates a proton
+    11: 3, // accepts a proton
+    12: 1  // HI and I-
+  },
+
+  "CH_U08_A20_P": {
+    1: 1, // Zn(s) (Active metal above H2 in Activity Series)
+    2: 3, // neutralization (Acid + Base reaction)
+    3: 3, // KOH (A base reacts with an acid to form salt and water)
+    4: 1, // KCl (Ionic compound, not an acid or base)
+    5: 1, // Yes (Calcium is above Hydrogen)
+    6: 1, // Yes (Lead is above Hydrogen)
+    7: { type: "frq" }, // H2SO4 + Mg(OH)2 -> MgSO4 + 2H2O
+    8: { type: "frq" }  // 2H3PO4 + 3Ca(OH)2 -> Ca3(PO4)2 + 6H2O
+  },
+
+  "CH_U08_A21_G": {
+    1: 3, // zinc (Above H on Table J)
+    2: 0, // aluminum (Highest on Table J among options)
+    3: 3, // neutralization
+    4: 0, // neutralization
+    5: 0, // 1.0 mol (1:1 molar ratio)
+    6: 3, // H+ + OH- -> H2O
+    7: 0, // water
+    8: 3, // a salt and water
+    9: 0, // Na3PO4 (Ionic compound)
+    10: 2, // decreases/remains same (OH- reacts; Na+ is spectator)
+    11: 3, // sulfuric acid (Strong electrolyte)
+    12: 0, // potassium chloride
+    13: 3, // Na+ and Cl-
+    14: 2, // a salt
+    15: { type: "frq" }, // HNO3 + NaOH -> NaNO3 + H2O
+    16: 0, // Yes (Al is above H)
+    17: { type: "frq" }, // H2SO4 + 2KOH -> K2SO4 + 2H2O
+    18: 1  // No (Cu is below H)
   }
+
 };
